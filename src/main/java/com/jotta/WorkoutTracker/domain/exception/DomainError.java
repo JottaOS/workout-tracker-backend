9df747d1857fe9707f404c6fa8676efc476b65c8
JottaOS <juanmendozasfa@gmail.com>
2025-11-02 15:00:43 -0300

    feat(exception): add error codes

--- src/main/java/com/jotta/WorkoutTracker/domain/exception/DomainError.java
+++ src/main/java/com/jotta/WorkoutTracker/domain/exception/DomainError.java
@@ -6,7 +6,16 @@ import lombok.Getter;
 public enum DomainError {
     INTERNAL_ERROR("WTB-500", "Internal domain error"),
     MUSCLE_NOT_FOUND("WTB-001", "Muscle not found"),
-    EXERCISE_NOT_FOUND("WTB-002", "Exercise not found");
+    EXERCISE_NOT_FOUND("WTB-002", "Exercise not found"),
+    WORKOUT_NOT_FOUND("WTB-003", "Workout not found"),
+    INVALID_WORKOUT_TIME_PERIOD("WTB-004", "Invalid workout time period"),
+    INVALID_BODYWEIGHT_SET("WTB-005", "Invalid bodyweight set"),
+    INVALID_WEIGHT_SET("WTB-006", "Invalid weight set"),
+    INVALID_CARDIO_SET("WTB-007", "Invalid cardio set"),
+    INVALID_CARDIO_WITH_DISTANCE_SET("WTB-008", "Invalid cardio with distance set"),
+    REPEATING_WORKOUT_EXERCISES("WTB-009", "Workouts cannot have repeated exercises"),
+    EMPTY_WORKOUT_EXERCISES("WTB-010", "Workouts require at least 1 exercise");
+
 
     private final String code;
     private final String message;
